479d042da6d8bcd3c126d270fdd0e5ad10a819a2
Rowan Dixon <jaydxn21@gmail.com>
2024-08-12 23:04:53 -0500

    Add delete and update buttons to transaction list and display in table

--- finance_frontend/src/components/Transaction/TransactionList.js
+++ finance_frontend/src/components/Transaction/TransactionList.js
@@ -1,21 +1,10 @@
 import React, { useState, useEffect } from "react";
-// import TransactionService from "../../services/transaction.service";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import "../style/transactionList.css";
 
 const TransactionList = () => {
   const [transactions, setTransactions] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchTransactions = async () => {
-  //     try {
-  //       const response = await TransactionService.getAll();
-  //       console.log("API response:", response);
-  //       setTransactions(response.data);
-  //     } catch (error) {
-  //       console.error(error);
-  //       console.error("Error fetching transactions:", error);
-  //     }
-  //   };
 
   useEffect(() => {
     const fetchTransactions = async () => {
@@ -41,17 +30,74 @@ const TransactionList = () => {
     fetchTransactions();
   }, []);
 
+  const handleDelete = async (transactionId) => {
+    const token = localStorage.getItem("token");
+
+    try {
+      await axios.delete(
+        "http://localhost:8080/api/transaction/${transactionId}",
+        {
+          headers: { "x-access-token": token },
+        }
+      );
+      setTransactions(transactions.filter((t) => t.id !== transactionId));
+    } catch (error) {
+      console.error("Error deleting transaction:", error);
+    }
+  };
+
+  // return (
+  //   <div>
+  //     <h2>Transaction List</h2>
+  //     <ul>
+  //       {transactions.map((transaction) => (
+  //         <li key={transaction.id}>
+  //           {transaction.amount} - {transaction.type} - {transaction.date} -{" "}
+  //           {transaction.description}
+  //         </li>
+  //       ))}
+  //     </ul>
+  //   </div>
+  // );
+
   return (
     <div>
       <h2>Transaction List</h2>
-      <ul>
-        {transactions.map((transaction) => (
-          <li key={transaction.id}>
-            {transaction.amount} - {transaction.type} - {transaction.date} -{" "}
-            {transaction.description}
-          </li>
-        ))}
-      </ul>
+      <table>
+        <thead>
+          <tr>
+            <th>Amount</th>
+            <th>Type</th>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.amount}</td>
+              <td>{transaction.type}</td>
+              <td>{new Date(transaction.date).toLocaleDateString()}</td>
+              <td>{transaction.description}</td>
+              <td>
+                <Link
+                  to={`/update-transaction/${transaction.id}`}
+                  className="update-button"
+                >
+                  Update
+                </Link>
+                <button
+                  onClick={() => handleDelete(transaction.id)}
+                  className="delete-button"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
