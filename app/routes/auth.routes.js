755b23aaa8c0aa68ab7925d9c7ecc8988b3d9283
Rowan Dixon <jaydxn21@gmail.com>
2024-10-26 17:14:44 -0500

    update auth route

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,7 +1,11 @@
 const auth = require("../controllers/auth.controller.js");
 var router = require("express").Router();
 
-router.post("/register", auth.register);
+router.post(
+  "/register",
+  cors({ origin: "https://fnce.onrender.com" }),
+  auth.register
+);
 router.post("/login", auth.login);
 router.get("/", auth.get);
 
